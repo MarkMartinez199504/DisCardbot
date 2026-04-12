68a9916b6c7433f3e762991abc17cc78a6e5b8e7
MarkMartinez199504 <ruinslasher2@gmail.com>
2026-04-12 10:41:35 -0400

    restructured code, added a tester slash command

--- bot.py
+++ bot.py
@@ -1,26 +1,41 @@
 import discord
+from discord.ext import commands
+from discord import app_commands
 from dotenv import load_dotenv
 import os
 
-intents = discord.Intents.default()
-intents.message_content = True
+class Client(commands.Bot):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
 
-client = discord.Client(intents=intents)
+        try:
+           guild = discord.Object(id=os.getenv('DEV_SERVER_ID'))
+           synced = await self.tree.sync(guild=guild)
+           print(f'Synced {len(synced)} commands to guild {guild.id}')
+        except Exception as e:
+            print(f'Error syncing commands: {e}')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+client = Client(command_prefix="lmao", intents=intents)
 
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
+GUILD_ID = discord.Object(id=os.getenv('DEV_SERVER_ID'))
 
-@client.event
-async def on_ready():
-    print(f"We have logged in as {client.user}")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.tree.command(name="hello", description="Say Hello!", guild=GUILD_ID)
+async def say_hello(interaction: discord.Interaction):
+    await interaction.response.send_message("Hi there!")
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+
 
 client.run(TOKEN)
